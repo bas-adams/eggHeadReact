76e5dc74f7908b2b3238563482eb5fe07c53ecef
Basia <basia.adamska@gmail.com>
2017-01-05 12:53:03 +0100

    v0

---
+++ src/js/components/RootView.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class RootView extends React.Component {
+
+	render() {
+		return (
+			<div className="RootView">
+				Powodzenia!
+			</div>
+		);
+	}
+}
